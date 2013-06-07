7a7d9658f8e75461ec4705569feab9a2d330b249
Ryan Olson <ryan.olson.x@gmail.com>
2013-06-07 18:12:39 -0400

    Added gravatar service, renamed event data service to eventData

--- js/controllers/EditProfileController.js
+++ js/controllers/EditProfileController.js
@@ -1,7 +1,10 @@
 "use strict";
 
 eventsApp.controller("EditProfileController",
-    function EditProfileController($scope) {
-	$scope.user = {};
-    }    
+    function EditProfileController($scope, gravatarUrlBuilder) {
+    	$scope.user = {};
+        $scope.getGravatarUrl = function(email) {
+            return gravatarUrlBuilder.buildGravatarUrl(email);
+        };
+    }
 );
